647c15975fdb2bde414956e9f5844c1be660c13a
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-10-08 09:24:50 +0530

    Improve error with bad cron timer syntax.

--- config/config-api/src/com/thoughtworks/go/config/TimerConfig.java
+++ config/config-api/src/com/thoughtworks/go/config/TimerConfig.java
@@ -1,26 +1,26 @@
-/*************************GO-LICENSE-START*********************************
- * Copyright 2014 ThoughtWorks, Inc.
+/*
+ * Copyright 2015 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *************************GO-LICENSE-END***********************************/
+ */
 
 package com.thoughtworks.go.config;
 
-import java.util.Map;
-
 import com.thoughtworks.go.domain.ConfigErrors;
-import com.thoughtworks.go.util.command.StringArgument;
 import org.quartz.CronExpression;
+
+import java.text.ParseException;
+import java.util.Map;
 
 
 /**
@@ -78,9 +78,10 @@ public class TimerConfig implements Validatable {
     }
 
     public void validate(ValidationContext validationContext) {
-        boolean isValid = CronExpression.isValidExpression(timerSpec);
-        if (!isValid) {
-            errors.add(TIMER_SPEC, "Invalid cron syntax");
+        try {
+            new CronExpression(timerSpec);
+        } catch (ParseException pe) {
+            errors.add(TIMER_SPEC, "Invalid cron syntax: " + pe.getMessage());
         }
     }
 
